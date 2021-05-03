25d5c56c6894292dc5f3b078a4854ddf2b321d79
artikgupta <artikgupta>
2021-05-03 21:17:10 +0530

    completed assignment II

---
+++ block-BNaadm/models/comment.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Article = require('./article');
+
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema(
+  {
+    content: { type: String, required: true },
+    articleId: { type: Schema.Types.ObjectId, ref: 'Article', required: true },
+    likes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+var Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
